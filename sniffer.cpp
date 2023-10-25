ea14ab40ae8f6566988bcc39a6a1e1a4a3c1b5ba
Subhajit009iitr <biswassubhajit009@gmail.com>
2023-10-25 22:55:37 +0530

    sniffer.cpp run through main

--- sniffer.cpp
+++ sniffer.cpp
@@ -4,7 +4,6 @@
 #include<stdio.h>	//For standard things
 #include<stdlib.h>	//malloc
 #include<string.h>	//strlen
-
 #include<netinet/ip_icmp.h>	//Provides declarations for icmp header
 #include<netinet/udp.h>	//Provides declarations for udp header
 #include<netinet/tcp.h>	//Provides declarations for tcp header
@@ -17,7 +16,18 @@
 #include<sys/time.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include<signal.h>
+#include<iostream>
 
+bool shouldRun = true;  // Initialize a flag
+
+void SignalHandler(int signum) {
+    if (signum == SIGINT) {
+        shouldRun = false;
+        printf("Received SIGINT. Stopping the capturing of packets.....................................\r\n");
+        return;
+    }
+}
 void ProcessPacket(unsigned char* , int);
 void print_ip_header(unsigned char* , int);
 void print_tcp_packet(unsigned char * , int );
@@ -28,47 +38,6 @@ void PrintData (unsigned char* , int);
 FILE *logfile;
 struct sockaddr_in source,dest;
 int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0,i,j;	
-
-int main()
-{
-	int saddr_size , data_size;
-	struct sockaddr saddr;
-		
-	unsigned char *buffer = (unsigned char *) malloc(65536); //Its Big!
-	
-	logfile=fopen("log.txt","w");
-	if(logfile==NULL) 
-	{
-		printf("Unable to create log.txt file.");
-	}
-	printf("Starting...\n");
-	
-	int sock_raw = socket( AF_PACKET , SOCK_RAW , htons(ETH_P_ALL)) ;
-	//setsockopt(sock_raw , SOL_SOCKET , SO_BINDTODEVICE , "eth0" , strlen("eth0")+ 1 );
-	
-	if(sock_raw < 0)
-	{
-		//Print the error with proper message
-		perror("Socket Error");
-		return 1;
-	}
-	while(1)
-	{
-		saddr_size = sizeof saddr;
-		//Receive a packet
-		data_size = recvfrom(sock_raw , buffer , 65536 , 0 , &saddr , (socklen_t*)&saddr_size);
-		if(data_size <0 )
-		{
-			printf("Recvfrom error , failed to get packets\n");
-			return 1;
-		}
-		//Now process the packet
-		ProcessPacket(buffer , data_size);
-	}
-	close(sock_raw);
-	printf("Finished");
-	return 0;
-}
 
 void ProcessPacket(unsigned char* buffer, int size)
 {
@@ -326,4 +295,46 @@ void PrintData (unsigned char* data , int Size)
 			fprintf(logfile ,  "\n" );
 		}
 	}
+}
+
+int main()
+{
+	int saddr_size , data_size;
+	struct sockaddr saddr;
+	
+	unsigned char *buffer = (unsigned char *) malloc(65536); //Its Big!
+	signal(SIGINT, SignalHandler);
+	logfile=fopen("log.txt","w");
+	if(logfile==NULL) 
+	{
+		printf("Unable to create log.txt file.");
+	}
+	printf("Starting...\n");
+	
+	int sock_raw = socket( AF_PACKET , SOCK_RAW , htons(ETH_P_ALL)) ;
+	//setsockopt(sock_raw , SOL_SOCKET , SO_BINDTODEVICE , "eth0" , strlen("eth0")+ 1 );
+	
+	if(sock_raw < 0)
+	{
+		//Print the error with proper message
+		perror("Socket Error");
+		return 1;
+	}
+	while(shouldRun)
+	{
+		saddr_size = sizeof saddr;
+		//Receive a packet
+		data_size = recvfrom(sock_raw , buffer , 65536 , 0 , &saddr , (socklen_t*)&saddr_size);
+		if(data_size <0 )
+		{
+			printf("Recvfrom error , failed to get packets\n");
+			return 1;
+		}
+		//Now process the packet
+		ProcessPacket(buffer , data_size);
+	}
+    close(sock_raw);
+	printf("```````Packets processed and log file generated``````` \n\r");
+	printf("Finished\n");
+	return 0;
 }
